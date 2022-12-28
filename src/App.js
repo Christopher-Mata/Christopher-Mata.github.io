a14b122ff489c71af3d60cd465d0bcc5cdb4fd7c
Christopher-Mata <christophermataaldana@gmail.com>
2022-12-28 13:53:56 -0600

    fixed url issue

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/resume-website' element={<Home/>} />
       </Routes>
     </Router>
 
